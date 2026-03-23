9a9221e289677d002a49e8da2be65fff4d7883fd
jnllyng <jueunyang00@gmail.com>
2026-03-23 13:08:41 -0500

    Test bad code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import type { ButtonProps } from './Button.types';
 
+const test = 'Test';
 const StyledButton = styled.button<{ $disabled?: boolean; $bg?: string }>`
   padding: 12px 16px;
   border-radius: 10px;
